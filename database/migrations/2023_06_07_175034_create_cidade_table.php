598575093b730ce9ddcc794067911191f3c7f585
Joao Gustavo <jgustavomendonca@gmail.com>
2023-06-07 16:03:50 -0300

    feat: :sparkles: Criando tabelas no banco de dados

--- database/migrations/2023_06_07_175034_create_cidade_table.php
+++ database/migrations/2023_06_07_175034_create_cidade_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('cidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('cidade');
+            $table->unsignedBigInteger('id_estado');
+            $table->foreign('id_estado')->references('id')->on('estados');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('cidades');
     }
 };
--- database/migrations/2023_06_07_183858_create_usuarios-hobbies_table.php
+++ database/migrations/2023_06_07_183858_create_usuarios-hobbies_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios-hobbies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->unsignedBigInteger('id_hobbie');
+            $table->foreign('id_hobbie')->references('id')->on('hobbies');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios-hobbies');
     }
 };
